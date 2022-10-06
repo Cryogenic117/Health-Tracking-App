99f402487f66feb0b97fdc20692bb63b6ae797aa
Conroy Ricketts <conroyr41@gmail.com>
2022-10-06 10:08:00 -0400

    Added a basic calendar.

--- src/screens/Calendar.tsx
+++ src/screens/Calendar.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Calendar as MainCalendar, CalendarList, Agenda } from 'react-native-calendars';
 
 export default function Calendar(): JSX.Element {
     return (
         <View>
+            <MainCalendar onDayPress={() => {/*Render the detailed information for that day*/}}/>
         </View>
     );
 }
