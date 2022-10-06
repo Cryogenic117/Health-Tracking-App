cd9f2a2f88d249b3606690c524ae4775113dcd4b
Conroy Ricketts <conroyr41@gmail.com>
2022-10-06 13:06:19 -0400

    Added a header for the daily data.

--- src/screens/Calendar.tsx
+++ src/screens/Calendar.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Calendar as MainCalendar, CalendarList, Agenda } from 'react-native-calendars';
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { Calendar as MainCalendar } from 'react-native-calendars'
 
 export default function Calendar(): JSX.Element {
     return (
         <View>
             <MainCalendar onDayPress={() => {/*Render the detailed information for that day*/}}/>
+            <View style={styles.dailyDataHeader}>
+                <Text style={styles.dailyDataHeaderText}>Daily Data</Text>
+                <View style={styles.dailyDataHeaderLineSeperator}/>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    dailyDataHeader: {
+        alignItems: 'center',
+        padding: 10
+    },
+    dailyDataHeaderText: {
+        fontSize: 20
+    },
+    dailyDataHeaderLineSeperator: {
+        borderBottomColor: "black", 
+        borderBottomWidth: 1, 
+        width: '100%',
+        paddingTop: 10
+    }
+})
