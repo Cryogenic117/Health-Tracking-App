7801c4d00ded379622e7f4ebb397725e86304039
Brandon Knudson <bknudson117@gmail.com>
2022-10-07 15:30:04 -0400

    Added Drop Down picker, still in progress

--- src/screens/Calendar.tsx
+++ src/screens/Calendar.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { Calendar as MainCalendar } from 'react-native-calendars'
+import DropDownPicker from 'react-native-dropdown-picker'
 
 export default function Calendar(): JSX.Element {
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(null);
+    const [items, setItems] = useState([
+        {label: 'Week', value: 'week'},
+        {label: 'Month', value: 'month'}
+    ]);
     return (
         <View>
+            <View style={styles.sectionBreak}/>
+            <DropDownPicker
+            open={open}
+            value={value}
+            items={items}
+            setOpen={setOpen}
+            setValue={setValue}
+            setItems={setItems}
+            style={styles.viewPicker}
+            />
             <MainCalendar onDayPress={() => {/*Render the detailed information for that day*/}}/>
             <View style={styles.dailyDataHeader}>
                 <Text style={styles.dailyDataHeaderText}>Daily Data</Text>
@@ -27,5 +44,14 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1, 
         width: '100%',
         paddingTop: 10
+    },
+    sectionBreak: {
+        width: '100%',
+        paddingTop: 50
+    },
+    viewPicker: {
+        width: '40%',
+        height: '10%',
+        left: 5
     }
 })
