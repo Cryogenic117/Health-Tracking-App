cfbce3e143f5b714d24881d31871cc83da38079c
Conroy Ricketts <conroyr41@gmail.com>
2022-10-15 19:07:48 -0400

    Tweaked placeholder for search bar.
    Tweaked the styling of the medication list.
    
    Formatted the code.

--- src/screens/Calendar.tsx
+++ src/screens/Calendar.tsx
@@ -1,68 +1,31 @@
-import React,{useState} from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { Calendar as MainCalendar, CalendarList, Agenda } from 'react-native-calendars'
-// import DropDownPicker from 'react-native-dropdown-picker'
+import React from 'react'
+import { StatusBar, StyleSheet, Text, View } from 'react-native'
+import { Agenda } from 'react-native-calendars'
 
 export default function Calendar(): JSX.Element {
-    const [open, setOpen] = useState(false)
-    const [value, setValue] = useState(null)
-    const [items, setItems] = useState([
-        {label: 'Week', value: 'week'},
-        {label: 'Month', value: 'month'}
-    ]);
     return (
-        <View>
-            <View style={styles.sectionBreak}/>
-            {/* <DropDownPicker
-             open={open}
-             value={value}
-             items={items}
-             setOpen={setOpen}
-             setValue={setValue}
-             placeholder="Month"
-             hideSelectedItemIcon={true}
-             setItems={setItems}
-             style={styles.viewPicker}
-             /> */}
-            <View style={styles.agenda}>                
-                <Agenda 
-                renderList={() => {
-                    return (
-                        <View style={styles.dailyDataHeader}>
-                            <Text style={styles.dailyDataHeaderText}>Daily Data</Text>
-                            <View style={styles.dailyDataHeaderLineSeperator}/>
-                        </View>
-                    )
-                }}/>
+        <View style={{marginTop: StatusBar.currentHeight}}>
+            <View style={{height: '100%'}}>                
+                <Agenda renderList={() => DailyData()}/>
             </View>
         </View>
-    );
+    )
+}
+
+function DailyData(): JSX.Element {
+    return (
+        <View style={{alignItems: 'center', padding: 10}}>
+            <Text style={{fontSize: 20}}>Daily Data</Text>
+            <View style={styles.lineSeperator}/>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-    agenda: {
-        height: '100%'
-    },
-    dailyDataHeader: {
-        alignItems: 'center',
-        padding: 10
-    },
-    dailyDataHeaderText: {
-        fontSize: 20
-    },
-    dailyDataHeaderLineSeperator: {
+    lineSeperator: {
         borderBottomColor: "black", 
         borderBottomWidth: 1, 
         width: '100%',
         paddingTop: 10
-    },
-    sectionBreak: {
-        width: '100%',
-        paddingTop: 50
-    },
-    viewPicker: {
-        width: '30%',
-        height: '10%',
-        left: 5
     }
 })
--- src/screens/Medication.tsx
+++ src/screens/Medication.tsx
@@ -1,57 +1,45 @@
 import React from 'react'
-import { View, Text, StyleSheet, StatusBar,ScrollView } from 'react-native'
+import { ScrollView, StatusBar, Text, View } from 'react-native'
 import SearchBar from "react-native-dynamic-search-bar"
-import NotesButton from '../components/NotesButton';
+import NotesButton from '../components/NotesButton'
 
 export default function Medication(): JSX.Element {
     return (
-        <View style={styles.container}>
-            <SearchBar />
-            <Text style={styles.header}>{"Your Medications:"}</Text>
-            <ScrollView style={styles.medList}>
-                {dataList.map(medName => (
-                    <View style={styles.medListItem}>
-                        <Text key={medName}>{medName}</Text>
-                        <NotesButton/>
-                    </View>
-            ))}
-            </ScrollView>
+        <View style={{marginTop: StatusBar.currentHeight, marginBottom: 200}}>
+            <SearchBar placeholder='Add a Medication'/>
+            <View style={{paddingHorizontal: 20}}>
+                <Text style={{fontSize: 30, paddingVertical: 10}}>Your Medications</Text>
+                <ScrollView>
+                    {mockData.map((medName, index) => (
+                        <View key={index} style={{paddingVertical: 10}}>
+                            <Text key={medName} style={{fontSize: 20, paddingBottom: 5}}>{medName}</Text>
+                            <NotesButton/>
+                        </View>
+                    ))}
+                </ScrollView>
+            </View>
         </View>
-    );
+    )
 }
 
-const styles=StyleSheet.create({
-    container: {
-        marginTop: StatusBar.currentHeight
-    },
-    header: {
-        fontSize: 30,
-        paddingTop: 10,
-        paddingLeft: 15
-    },
-    medListItem: {
-        paddingTop: 25,
-        paddingLeft: 15
-    },
-    medList: {
-        
-    }
-})
-
-const dataList: string[] = [
+const mockData: string[] = [
     "Diazepam (Valium)",
     "Metformin (Riomet)",
     "Mutli-Vitamins",
-    "Iron Supplement",   "Diazepam (Valium)",
+    "Iron Supplement",
+    "Diazepam (Valium)",
     "Metformin (Riomet)",
     "Mutli-Vitamins",
-    "Iron Supplement",   "Diazepam (Valium)",
+    "Iron Supplement",
+    "Diazepam (Valium)",
     "Metformin (Riomet)",
     "Mutli-Vitamins",
-    "Iron Supplement",   "Diazepam (Valium)",
+    "Iron Supplement",
+    "Diazepam (Valium)",
     "Metformin (Riomet)",
     "Mutli-Vitamins",
-    "Iron Supplement",   "Diazepam (Valium)",
+    "Iron Supplement",
+    "Diazepam (Valium)",
     "Metformin (Riomet)",
     "Mutli-Vitamins",
     "Iron Supplement"
