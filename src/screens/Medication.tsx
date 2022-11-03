7978d513204628b45ddf1da2852e481d9fff1ff4
Conroy Ricketts <conroyr41@gmail.com>
2022-11-03 19:53:40 -0400

    Got the modal to show on search result press. However, showing the results is still WIP.

--- src/screens/Medication.tsx
+++ src/screens/Medication.tsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
 import { ScrollView, StatusBar, Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 import SearchBar from "react-native-dynamic-search-bar"
+import NewMedicationModal from '../components/NewMedicationModal'
 import NotesButton from '../components/NotesButton'
 
 export default function Medication(): JSX.Element {
-    const [searchResults, setSearchResults] = useState([]) 
+    const [isSearchActive, setSearchActive] = useState(false)
+    const [selectedMedication, setSelectedMedication] = useState("")
+    const [showModal, setShowModal] = useState(false)
+
+    let currentQuery: string = ""
+    let searchResults: string[] = []
     
     const searchRx = (searchTerm: string) => {
         fetch(`https://rxnav.nlm.nih.gov/REST/RxTerms/allconcepts.json`)
@@ -26,42 +32,70 @@ export default function Medication(): JSX.Element {
                 })
                 return searchResultIDs
             })
-            .then((searchResultIDs) => {                
+            .then((searchResultIDs) => {      
                 let searchResultDisplayNames = []
                 searchResultIDs.forEach((ID) => {
                     fetch(`https://rxnav.nlm.nih.gov/REST/RxTerms/rxcui/${ID}/name.json`)
                         .then((response) => response.json())
-                        .then((data) => console.log(data.displayGroup.displayName))
+                        .then((data) => searchResultDisplayNames.push(data.displayGroup.displayName))
                 })
-                setSearchResults(searchResultDisplayNames)
+                searchResults = searchResultDisplayNames
             })
     }
 
-    searchRx("diazepam")
+    const onNewQuery = (queryText: string) => {
+        if (queryText.length != 0 && !isSearchActive) {
+            setSearchActive(true)
+        }
+        currentQuery = queryText
+    }
+
+    const onClearSearch = () => {
+        setSearchActive(false)
+    }
+
+    const onSearchButtonPress = () => {  
+        console.log("X         " + currentQuery)      
+        searchRx(currentQuery)
+    }
 
     return (
         <View style={{marginTop: StatusBar.currentHeight, marginBottom: 200}}>
-            {/* <SearchBar placeholder='Add a Medication'/>
-            <View style={{paddingHorizontal: 20}}>
-                <Text style={{fontSize: 30, paddingVertical: 10}}>Your Medications</Text>
+            <SearchBar 
+                placeholder='Add a Medication'
+                onChangeText={(text) => onNewQuery(text)}
+                onClearPress={() => onClearSearch()}                
+                onSearchPress={() => onSearchButtonPress()}
+            />
+            {!isSearchActive && 
+                <View style={{paddingHorizontal: 20}}>
+                    <Text style={{fontSize: 30, paddingVertical: 10}}>Your Medications</Text>
+                    <ScrollView>
+                        {userMedications.map((medName, index) => (
+                            <View key={index} style={{paddingVertical: 10}}>
+                                <Text key={medName} style={{fontSize: 20, paddingBottom: 5}}>{medName}</Text>
+                                <NotesButton/>
+                            </View>
+                        ))}
+                    </ScrollView>
+                </View>
+            }
+            {isSearchActive &&
                 <ScrollView>
-                    {userMedications.map((medName, index) => (
-                        <View key={index} style={{paddingVertical: 10}}>
-                            <Text key={medName} style={{fontSize: 20, paddingBottom: 5}}>{medName}</Text>
-                            <NotesButton/>
-                        </View>
+                    {searchResults.map((searchResult, index) => (
+                        <TouchableOpacity key={index} style={styles.searchResultCard} onPress={() => {
+                            console.log("i was pressed")
+                            setSelectedMedication(searchResult)
+                            setShowModal(true)
+                        }}>
+                            <Text style={styles.searchResultCardText}>
+                                {searchResult}
+                            </Text>
+                        </TouchableOpacity>
                     ))}
                 </ScrollView>
-            </View> */}
-            <ScrollView>
-                {userMedications.map((searchResult,index) => (
-                    <TouchableOpacity key = {index} style = {styles.searchResultCard} onPress = {() => {}}>
-                        <Text style = {styles.searchResultCardText}>
-                            {searchResult}
-                        </Text>
-                    </TouchableOpacity>
-                ))}
-            </ScrollView>
+            }
+            <NewMedicationModal medicationName={selectedMedication} toggle={() => {setShowModal(!showModal)}} isOpen={showModal}/>
         </View>
     )
 }
@@ -70,7 +104,7 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         borderWidth: 2,
         height: 50,
-        margin: 10,
+        margin: 10
     },
     searchResultCardText: {
         textAlign: 'center',
