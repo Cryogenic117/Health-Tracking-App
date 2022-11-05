d56cf4c8ad2ef1fb3d8fc44b8cb6f0d3eafa8d0a
Cryogenic117 <bknudson117@gmail.com>
2022-11-05 14:43:10 -0400

    Fixed calendar bug and changed format

--- src/screens/Calendar.tsx
+++ src/screens/Calendar.tsx
@@ -1,51 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { StatusBar, StyleSheet, Text, View, ScrollView } from 'react-native'
-import { Agenda } from 'react-native-calendars'
+import { Agenda, CalendarProvider } from 'react-native-calendars'
 
 export default function Calendar(): JSX.Element {
-    var currentDateSelected = "2022-11-04"
-
+    var date = new Date();
+    const initialDate = date.toISOString().substr(0,10)
+	var currentDateSelected = initialDate
     return (
         <View style={{marginTop: StatusBar.currentHeight}}>
-            <View style={{height: '100%'}}>                
+            <View style={{height: '100%'}}>
+                <CalendarProvider date={initialDate}>             
                 <Agenda renderList={() => DailyData(currentDateSelected)}
-                    onDayPress={day => {
-                            console.log('day pressed',day.dateString)
-                            currentDateSelected = day.dateString
+                    onDayPress={day => {   
+                        currentDateSelected = day.dateString  
                     }}
-                      theme={{
+                    hideExtraDays={true}
+                    theme={{
                         agendaKnobColor: '#5838B4',
                         calendarBackground: 'black',
                         selectedDayBackgroundColor: "#5838B4",
-                        todayTextColor: '#5838B4'
-                      }}
+                        monthTextColor: 'white',
+                        todayTextColor: '#5838B4',
+                        dayTextColor: 'white'
+                    }}
                 />
+                </CalendarProvider> 
             </View>
         </View>
     )
 }
-// const Data1 = {
-//     // Loaded daily
-//     medications: ["Diazepam (Valium)", "Metformin (Riomet)", "Mutli-Vitamins", "Iron Supplement"],
-//     medicationNotes: ["Used to treat Anxiety, muscle spasms, and seizures.", "It can treat type 2 diabetes.", "Good for staying healthy", "For people with iron deficiency"],
-//     sleepDuration: 8,
-//     sleepQuality: 10,
-//     mood: ["Happy","Energy"],
-//     moodLevel: [10,6]
-// }
-// const Data2 = {
-//     medications: ["Empty"],
-//     medicationNotes: ["Used to treat Anxiety, muscle spasms, and seizures.", "It can treat type 2 diabetes.", "Good for staying healthy", "For people with iron deficiency"],
-//     sleepDuration: 8,
-//     sleepQuality: 10,
-//     mood: ["Happy","Energy"],
-//     moodLevel: [10,6]
-// }
-// const Storage = {
-//     "2022-11-04": Data2,
-//     "2022-11-05": Data1
-// }
-
 
 function DailyData(day): JSX.Element {
 
@@ -54,13 +37,8 @@ function DailyData(day): JSX.Element {
             <Text style={{textAlign: 'center', fontSize: 20}}>Daily Data</Text>
             <View style={styles.lineSeperator}/>
                 <ScrollView>
-                    {
-                    Storage[day] != undefined &&
-                    Storage[day].medications.map((medName, index) => (
-                        <View key={index}>
-                            <Text key={medName} style={{textAlign: 'left', fontSize: 12, paddingBottom: 5}}>Take {medName}</Text>
-                        </View>
-                    ))}
+                    {//Display data for Date
+                    }
                 </ScrollView>
         </View>
     )
