125e33a3bb131ac9cc4f5cd79a20ed4d53f68ad0
martucciv <vmartucci@knights.ucf.edu>
2022-11-06 11:31:47 -0500

    Fixed notes keyboard bug

--- src/components/NotesButton.tsx
+++ src/components/NotesButton.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react' 
-import {StyleSheet, Text, TouchableOpacity, View, Button, TextInput, KeyboardAvoidingView, ScrollView, TouchableWithoutFeedback} from 'react-native'
+import {StyleSheet, Text, TouchableOpacity, View, Button, TextInput, KeyboardAvoidingView, ScrollView} from 'react-native'
 import Modal from 'react-native-modal'
 
 export default function NotesButton(): JSX.Element {
