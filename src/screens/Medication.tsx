9a80caa2c3d038433053869108be3d859352cbef
Conroy Ricketts <conroyr41@gmail.com>
2022-11-12 15:51:46 -0500

    Reformatted code.

--- src/screens/Medication.tsx
+++ src/screens/Medication.tsx
@@ -1,66 +1,81 @@
-import React, { useState, Component } from 'react'
-import { ScrollView, SafeAreaView, Image, StatusBar, ActivityIndicator, Text, View, StyleSheet, TouchableOpacity, TextInput, Platform, Keyboard, TouchableWithoutFeedback } from 'react-native'
+import React, { useState } from 'react'
+import {
+    ActivityIndicator,
+    Keyboard,
+    Platform,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View
+} from 'react-native'
 import NewMedicationModal from '../components/NewMedicationModal'
 import NotesButton from '../components/NotesButton'
 
 export default function Medication(): JSX.Element {
-	const[results,setResults]= useState(null)
-	const[textEditable,setTextEditable]= useState(true)
+	const [results, setResults] = useState(null)
+	const [textEditable, setTextEditable] = useState(true)
     const [isSearchActive, setSearchActive] = useState(false)
     const [selectedMedication, setSelectedMedication] = useState("")
     const [showModal, setShowModal] = useState(false)
     const [userMedications, setUserMedications] = useState<string[]>([])
     const [text,setText] = useState("")
 
-    function search() {
+    const search = () => {
         const currentQuery = text
         setResults(<ActivityIndicator size="small" color="#0000ff" />)
-        try{
-        setTextEditable(false)
-        fetch("https://api.fda.gov/drug/label.json?search=openfda.brand_name:"+ currentQuery + '&limit=100')
-        .then(raw_data => raw_data.json())
-        .then(data => {
-            if (data.error){
-                const not_found_text = <Text style={styles.error}>The results you searched for were not found.</Text>
-                setResults(not_found_text)
-                return null
-            }
-    
-            // gets items to render from the output of the FDA API
-            let to_render = get_items(data)
-            // update what is rendering on this object based on the items above.
-            setResults(to_render)
-        })
-        .then(() => {setTextEditable(true)})
-    } catch(error) {
-        const error_text = <Text style={styles.error}>There was an error, try again.</Text>
-        setResults(error_text)
+        try {
+            setTextEditable(false)
+            fetch("https://api.fda.gov/drug/label.json?search=openfda.brand_name:" + currentQuery + '&limit=100')
+                .then(rawData => rawData.json())
+                .then(data => {
+                    if (data.error) {
+                        const notFoundText = <Text style={styles.error}>{"The results you searched for were not found."}</Text>
+                        setResults(notFoundText)
+                        return null
+                    }
+                    let itemsToRender = getItems(data)
+                    setResults(itemsToRender)
+                })
+                .then(() => setTextEditable(true))
+        } catch(error) {
+            const errorText = <Text style={styles.error}>{"There was an error, try again."}</Text>
+            setResults(errorText)
+        }
     }
-    }
-    function get_items(data){
+
+    const getItems = (data) => {
         return data.results.map((item) => {
-            return(
-                <TouchableOpacity key={item.id} onPress={() => 
-                resultPressed(item.openfda.brand_name[0].charAt(0).toUpperCase()+item.openfda.brand_name[0].substr(1).toLowerCase())}
+            return (
+                <TouchableOpacity 
+                    key={item.id} 
+                    onPress={() => 
+                        resultPressed(item.openfda.brand_name[0].charAt(0).toUpperCase() + item.openfda.brand_name[0].substr(1).toLowerCase())
+                    }
                 >
                     <View style={styles.item}>
-                            <Text style={styles.drugName}>
-                                {item.openfda.brand_name[0].charAt(0).toUpperCase()+item.openfda.brand_name[0].substr(1).toLowerCase()}
-                            </Text>
-                            <Text style={styles.drugInfo}>
-                                Generic: {item.openfda.generic_name[0]}
-                            </Text>
+                        <Text style={styles.drugName}>
+                            {item.openfda.brand_name[0].charAt(0).toUpperCase() + item.openfda.brand_name[0].substr(1).toLowerCase()}
+                        </Text>
+                        <Text style={styles.drugInfo}>
+                            {`Generic: ${item.openfda.generic_name[0]}`}
+                        </Text>
                     </View>
                 </TouchableOpacity>
             )
         })
     }
-    function resultPressed(medName) {
+
+    const resultPressed = (medName) => {
         setSelectedMedication(medName)
         setText("")
         setResults(null)
         setShowModal(true)
     }
+    
     const medicationModalCancelToggle = () => {
         setShowModal(!showModal)
     }
@@ -71,52 +86,56 @@ export default function Medication(): JSX.Element {
         setSearchActive(false)
     }
 
-    function cancelButtonPressed() {
+    const cancelButtonPressed = () => {
         setSearchActive(false)
         setResults(null)
         setText("")
         Keyboard.dismiss()
     }
-    return(
-            <SafeAreaView style={styles.AndroidSafeArea}>
-				<SafeAreaView style={{flex:1, margin: "auto", alignSelf: 'center', alignContent:"center", flexDirection:"row"}}>
-					<TextInput onChangeText={value => setText(value)}
-                        onPressIn={() => setSearchActive(true)}
-                        autoCorrect={false}
-                        value = {text}
-                        onSubmitEditing={()=> search()}
-						editable={textEditable}
-                        placeholder= "Add a medication"
-                        clearButtonMode='always'
-						style={styles.search}/>
-                    {isSearchActive &&
-                            <Text style={styles.cancel} onPress={() => cancelButtonPressed()}>C A N C E L</Text>
-                    }
-				</SafeAreaView>
-				<View style={{flex:6}}>
+
+    return (
+        <SafeAreaView style={styles.androidSafeArea}>
+            <SafeAreaView style={{flex: 1, margin: "auto", alignSelf: 'center', alignContent:"center", flexDirection:"row"}}>
+                <TextInput 
+                    onChangeText={value => setText(value)}
+                    onPressIn={() => setSearchActive(true)}
+                    autoCorrect={false}
+                    value={text}
+                    onSubmitEditing={()=> search()}
+                    editable={textEditable}
+                    placeholder="Add a medication"
+                    clearButtonMode='always'
+                    style={styles.search}
+                />
+                {isSearchActive &&
+                    <Text style={styles.cancel} onPress={() => cancelButtonPressed()}>{"C A N C E L"}</Text>
+                }
+            </SafeAreaView>
+            <View style={{flex:6}}>
                 {!isSearchActive &&
-                <View style={{flex:1}}>
-                    <Text style={{alignSelf: 'center', fontSize: 30}}>Your Medications</Text>
-                    <ScrollView style={{flex: 5}}>
-                        {userMedications.map((medName, index) => (
-                            <View key={index} style={{paddingVertical: 10, paddingLeft: 15}}>
-                                <Text key={medName} style={{fontSize: 20, paddingBottom: 5}}>{medName}</Text>
-                                <NotesButton/>
-                            </View>
-                        ))}
-                    </ScrollView>
-                </View>}
-					<ScrollView style={{flex:8}}>
-                            {results}
-					</ScrollView>
-				</View>
-                <NewMedicationModal 
+                    <View style={{flex:1}}>
+                        <Text style={{alignSelf: 'center', fontSize: 30}}>{"Your Medications"}</Text>
+                        <ScrollView style={{flex: 5}}>
+                            {userMedications.map((medName, index) => (
+                                <View key={index} style={{paddingVertical: 10, paddingLeft: 15}}>
+                                    <Text key={medName} style={{fontSize: 20, paddingBottom: 5}}>{medName}</Text>
+                                    <NotesButton/>
+                                </View>
+                            ))}
+                        </ScrollView>
+                    </View>
+                }
+                <ScrollView style={{flex:8}}>
+                        {results}
+                </ScrollView>
+            </View>
+            <NewMedicationModal 
                 medicationName={selectedMedication} 
                 cancelToggle={medicationModalCancelToggle}
                 saveToggle={medicationModalSaveToggle}
                 isOpen={showModal}
             />
-			</SafeAreaView>
+        </SafeAreaView>
     )
 }
 
@@ -130,7 +149,7 @@ const styles = StyleSheet.create({
 		fontWeight: "bold",
 	},
     drugInfo: {
-        fontSize:12
+        fontSize: 12
     },
     item: {
         flex: 3,
@@ -147,7 +166,7 @@ const styles = StyleSheet.create({
         backgroundColor: "white", 
         borderRadius: 10,
         borderWidth: 1.5,
-        borderColor: '#d3d3d3',
+        borderColor: '#d3d3d3'
     },
     cancel: {
         paddingTop: 12.5,
@@ -156,11 +175,11 @@ const styles = StyleSheet.create({
         maxWidth: '90%', 
         flex: 2, 
         textAlign: "center",
-        color: 'black',
+        color: 'black'
     },
-    AndroidSafeArea: {
+    androidSafeArea: {
         flex: 1,
         backgroundColor: "white",
         paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
-      }
+    }
 })
